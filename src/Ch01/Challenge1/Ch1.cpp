eaabfddfe762c8d6e4743fa741df8f7354092738
ArminBalderas <balderas@gmx.net>
2024-07-10 13:49:54 +0000

    Chapter1 code changes

---
+++ src/Ch01/Challenge1/Ch1.cpp
@@ -0,0 +1,14 @@
+//Challenge
+//read a name and print it back
+
+#include <iostream>
+#include <string>
+
+int main(void){
+    std::string str_input;
+    std::cout << "Dime tu nombre? " << std::endl;
+    std::cin >> str_input;
+    std::cout << "Hola " << str_input << std::endl;
+    std::cout << std::endl << std::endl;
+    return (0);
+}
--- src/Ch02/02_03b/CodeDemo.cpp
+++ src/Ch02/02_03b/CodeDemo.cpp
@@ -4,7 +4,16 @@
 
 #include <iostream>
 
+int a,b=5; // Global variables
+
 int main(){
+    bool b_myFlag;
+    a = 7;
+    b_myFlag = false;
+
+    std::cout << "a: " << a << std::endl;
+
+
     std::cout << "Hi There!" << std::endl;
     
     std::cout << std::endl << std::endl;
